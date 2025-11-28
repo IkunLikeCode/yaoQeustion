a769d31cf9e8bf9d24f22eb093f1328a863ef64f
yaobo <15170970773@163.com>
2025-11-28 20:04:48 +0800

    fix(LinkPanel): 将本地主机地址替换为服务器IP地址

--- src/page/Question/Statistics/components/LinkPanel/LinkPanel.vue
+++ src/page/Question/Statistics/components/LinkPanel/LinkPanel.vue
@@ -28,7 +28,7 @@ defineProps<{
       <el-input
         ref="inputRef"
         class="link"
-        :value="'http://localhost:3000/question/' + route.query.id"
+        :value="'http://115.190.61.20/question/' + route.query.id"
         placeholder="请输入搜索内容"
       ></el-input>
       <el-button type="primary" @click="copyLink">复制</el-button>
